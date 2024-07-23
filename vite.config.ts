5ffeb40f6215b7ceab2c3a69b5d0e5786bc982b3
Brian Adams <wreality@gmail.com>
2024-07-23 14:02:42 -0400

    fix build options

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { keycloakify } from "keycloakify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), keycloakify({})]
+    plugins: [react(), keycloakify({
+    })],
 });
